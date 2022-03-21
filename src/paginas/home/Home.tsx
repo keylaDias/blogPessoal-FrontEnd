52b81d202d2ea08c8d15b8a27d4f053364ee3dea
Keyla <Keyladias.dk@hotmail.com>
2022-03-21 17:38:11 -0300

    React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return (
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrXfbdfhPzEnJO8EDTW1rwEvsle5fKAduSJQ&usqp=CAU" alt="imagem tela inicial" className="img" />
+        </>
+    );
+}
+export default Home;
