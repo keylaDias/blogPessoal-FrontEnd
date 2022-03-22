89920a65227a33827bd47638b866a7e77558b1c1
Keyla <Keyladias.dk@hotmail.com>
2022-03-22 20:53:07 -0300

    Atividade React 3

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { Grid, Box, Typography, Button } from "@material-ui/core";
+
+//import {Box, Paper, Button} from "@material-ui/core";
 import "./Home.css";
 
-function Home(){
+function Home() {
     return (
         <>
-        <h1 className="titulo">Home</h1>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrXfbdfhPzEnJO8EDTW1rwEvsle5fKAduSJQ&usqp=CAU" alt="imagem tela inicial" className="img" />
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3f5185 " }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold " }} > Seja bem Vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold " }} >Expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1} >
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3f51b5", color: "wite" }}>Ver Postagens </Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Aula" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
