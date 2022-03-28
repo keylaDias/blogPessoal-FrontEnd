4ade239beca661ef35ba189aa3aa0ec22ec67016
Keyla <Keyladias.dk@hotmail.com>
2022-03-28 18:54:31 -0300

    atualizando blog

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,38 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
 import Home from "./paginas/home/Home";
-import Login from './paginas/login/Login';
+import Login from "./paginas/login/Login";
+import { Grid } from '@material-ui/core';
 import './App.css';
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
 
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <div style={{minHeight: "100vh"}}>
-        <Route path="/login">
-            <Login />
-          </Route>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <div style={{ minHeight: "100vh" }}>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
 
-          <Route path="/home">
-            <Home />
-          </Route>
-        </div>
-      </Switch>
-      <Footer />
-    </Router>
+            <Route path="/home">
+              <Home />
+            </Route>
+
+            <Route path="/cadastroUsuario">
+              <CadastroUsuario />
+            </Route>
+          </div>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Box } from "@material-ui/core";
-
+import {Link} from "react-router-dom";
 function Navbar() {
     return (
         <>
@@ -33,11 +33,13 @@ function Navbar() {
                                 Cadastrar Temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Link to="/login" className="text-decorator-none">
+                        <Box mx={1} style={{ cursor: "pointer" , color:"white" }}>
                             <Typography variant="h6" color="inherit" >
                                 Logout
                             </Typography>
                         </Box>
+                        </Link>
                     </Box>
                 </Toolbar>
             </AppBar>
